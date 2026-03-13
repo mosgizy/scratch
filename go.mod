5816c4472fe0f8217055175c132dd9b78ea42079
mosgizy <moshood521@gmail.com>
2026-03-13 12:01:53 +0100

    all project from tutorial completed

--- go.mod
+++ go.mod
@@ -7,4 +7,6 @@ require github.com/joho/godotenv v1.5.1
 require (
 	github.com/go-chi/chi v1.5.5 // indirect
 	github.com/go-chi/cors v1.2.2 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/lib/pq v1.11.2 // indirect
 )
--- main.go
+++ main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
+	"github.com/mosgizy/rssagg/internal/database"
+
+	_ "github.com/lib/pq"
 )
+
+type apiConfig struct{
+	DB *database.Queries
+}
 
 func main() {
 	godotenv.Load()
@@ -18,6 +27,24 @@ func main() {
 	if port == "" {
 		log.Fatal("PORT is not found in the environment")
 	}
+
+	dbUrl := os.Getenv("DB_URL")
+	if dbUrl == "" {
+		log.Fatal("DB_URL is not found in the environment")
+	}
+
+	conn, err := sql.Open("postgres",dbUrl)
+	if err != nil {
+		log.Fatal("Can't connect to database")
+	}
+
+	db := database.New(conn)
+
+	apiCfg := apiConfig{
+		DB: db,
+	}
+	
+	go startScrapping(db,10, time.Minute)
 
 	router := chi.NewRouter()
 
@@ -33,6 +60,17 @@ func main() {
 	v1Router := chi.NewRouter()
 	v1Router.Get("/healthz", handlerReadiness)
 	v1Router.Get("/err",handlerErr)
+	v1Router.Post("/users", apiCfg.handlerCreateUser)
+	v1Router.Get("/users", apiCfg.middlewareAuth(apiCfg.handlerGetUser))
+
+	v1Router.Post("/feeds",apiCfg.middlewareAuth(apiCfg.handlerCreateFeed))
+	v1Router.Get("/feeds",apiCfg.handlerGetFeeds)
+
+	v1Router.Get("/posts",apiCfg.middlewareAuth(apiCfg.handlerGetPostsForUser))
+
+	v1Router.Post("/feed_follows",apiCfg.middlewareAuth(apiCfg.handlerCreateFeedFollows))
+	v1Router.Get("/feed_follows",apiCfg.middlewareAuth(apiCfg.handlerGetFeedFollows))
+	v1Router.Delete("/feed_follows/{feedFollowID}",apiCfg.middlewareAuth(apiCfg.handlerDeleteFeedFollows))
 
 	router.Mount("/v1",v1Router)
 
@@ -43,7 +81,7 @@ func main() {
 
 	log.Printf("Server starting on port %v", port)
 
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
